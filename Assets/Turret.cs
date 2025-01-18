3f615745b9adcceb2f6474cb8af387cd3cf8aa8d
Shindaime-FH <fhb241578@fh-vie.ac.at>
2025-01-18 03:08:00 +0100

    Turret Rotation added
    And Sprites like the base of the turret were added + enemy layer added

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -5,10 +5,13 @@ public class Turret : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
 
+    private Transform target;
     private void OnDrawGizmosSelected()
     {
         Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
@@ -22,6 +25,36 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+    }
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation,targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
